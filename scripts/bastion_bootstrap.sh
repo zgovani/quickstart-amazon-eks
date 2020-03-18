a83b8a1d16c6be0f5fc320dd9c9de255d637070e
Luis Azevedo <labraceta@gmail.com>
2020-03-18 09:17:42 +0000

    Fix kubectl and aws-iam-authenticator being installed swapped on bastion

--- scripts/bastion_bootstrap.sh
+++ scripts/bastion_bootstrap.sh
@@ -426,13 +426,13 @@ EOF
 
 function install_kubernetes_client_tools() {
     mkdir -p /usr/local/bin/
-    retry_command 20 curl --retry 5 -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl
+    retry_command 20 curl --retry 5 -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
     chmod +x ./aws-iam-authenticator
     mv ./aws-iam-authenticator /usr/local/bin/
-    retry_command 20 curl --retry 5 -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
+    retry_command 20 curl --retry 5 -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl
     chmod +x ./kubectl
     mv ./kubectl /usr/local/bin/
-    cat > /etc//profile.d/kubectl.sh <<EOF
+    cat > /etc/profile.d/kubectl.sh <<EOF
 #!/bin/bash
 if [[ ":$PATH:" != *":/usr/local/bin:"* ]]; then     PATH="$PATH:/usr/local/bin";   fi
 source <(kubectl completion bash)
